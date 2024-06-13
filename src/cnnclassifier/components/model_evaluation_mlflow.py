5fc0077692ec85c8927f7e8f883963c56fae904f
Pallavi Padav <pallavi.padav@gmail.com>
2024-06-13 19:10:29 +0530

    eval2

--- src/cnnclassifier/components/model_evaluation_mlflow.py
+++ src/cnnclassifier/components/model_evaluation_mlflow.py
@@ -5,7 +5,8 @@ import mlflow.keras
 from urllib.parse import urlparse
 from cnnClassifier.entity.config_entity import EvaluationConfig
 from cnnClassifier.utils.common import read_yaml, create_directories,save_json
-
+import dagshub
+dagshub.init(repo_owner='PallaviPadav', repo_name='Kidney_Disease_Classification', mlflow=True)
 
 class Evaluation:
     def __init__(self, config: EvaluationConfig):
