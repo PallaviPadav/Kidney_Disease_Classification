bad2b6f4b6c376502f118c44c59555e1bb1e1857
Pallavi Padav <pallavi.padav@gmail.com>
2024-06-14 23:44:17 +0530

    mlfl2

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from src.cnnclassifier import logger
-
-from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from cnnClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
